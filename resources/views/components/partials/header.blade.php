6534920dc2f7f843092c0d25e1f3c11db64c467a
Atageldi Didarov <didarov.atageldi@gmail.com>
2025-02-13 14:38:43 +0500

    edit links

--- resources/views/components/partials/header.blade.php
+++ resources/views/components/partials/header.blade.php
@@ -1,21 +1,13 @@
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
     <div class="container">
-{{--        <a class="navbar-brand" href="{{ url('/') }}">--}}
-{{--            {{ config('app.name', 'Laravel') }}--}}
-{{--        </a>--}}
+        <a class="navbar-brand" href="{{ route('news.index') }}">
+            {{ __('News') }}
+        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav me-auto">
-                <!-- Special Menu Items -->
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('news.index') }}">{{ __('News') }}</a>
-                </li>
-            </ul>
-
             <!-- Right Side Of Navbar -->
             <ul class="navbar-nav ms-auto">
                 <!-- Authentication Links -->
