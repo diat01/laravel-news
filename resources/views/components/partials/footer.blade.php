5f01224e76818f353e3a7d5da0c3378991e3bb5f
Atageldi Didarov <didarov.atageldi@gmail.com>
2025-02-13 14:03:55 +0500

    edit footer

--- resources/views/components/partials/footer.blade.php
+++ resources/views/components/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="bg-white shadow-sm py-4 mt-5">
     <div class="container text-center">
-        <p class="mb-0">© {{ date('Y') }}. All rights reserved.</p>
+        <p class="mb-0">Developer: <a href="https://github.com/diat01" target="_blank">Atageldi Didarov</a></p>
     </div>
 </footer>
