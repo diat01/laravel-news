ae18e0483100a2be931eb7af540079b56dca3f34
Atageldi Didarov <didarov.atageldi@gmail.com>
2025-02-14 16:53:26 +0500

    define routes

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,15 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Route::resources([
+//    'news' => 'App\Http\Controllers\NewsController',
+//]);
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/profile/change-password',
+        \App\Livewire\Profile\ChangePassword::class)->name('profile.change-password');
+    Route::get('/profile/my-news', \App\Livewire\Profile\MyNews::class)->name('profile.my-news');
+});
+
+Route::get('/', \App\Livewire\News\NewsIndex::class)->name('news.index');
+Route::get('/news/{id}', \App\Livewire\News\NewsShow::class)->name('news.show');
